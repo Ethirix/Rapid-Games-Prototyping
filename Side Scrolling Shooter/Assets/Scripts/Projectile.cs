1f0b87f4145bde8b756932002a3712aba839f83a
Jack Wright <68773585+Ethirix@users.noreply.github.com>
2022-12-09 00:36:47 +0000

    Worked Continued on Health

--- Side Scrolling Shooter/Assets/Scripts/Projectile.cs
+++ Side Scrolling Shooter/Assets/Scripts/Projectile.cs
@@ -1,18 +1,58 @@
+using System.Collections;
 using UnityEngine;
 
-[RequireComponent(typeof(SpriteRenderer), typeof(PolygonCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Projectile : MonoBehaviour
 {
+    [Header("Projectile Settings")]
     [SerializeField] private int damage;
-    [SerializeField] private Sprite sprite;
-    public int Damage { get; private set; }
+    [SerializeField] private float cooldown;
+    [SerializeField] private float speed;
 
+    [Header("Object Settings")]
+    [SerializeField] private float time;
+    [SerializeField] private Sprite sprite;
+
+    public int Damage { get; private set; }
 
     private SpriteRenderer _spriteRenderer;
 
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
+        _spriteRenderer.sprite = sprite;
+        gameObject.AddComponent<PolygonCollider2D>();
+        gameObject.tag = "Projectile";
         Damage = damage;
+        StartCoroutine(DeletionTimer());
     }
+
+    private IEnumerator DeletionTimer()
+    {
+        float timer = 0;
+        while (true)
+        {
+            timer += Time.fixedDeltaTime;
+
+            if (timer > time)
+            {
+                Destroy(gameObject);
+            }
+
+            yield return new WaitForFixedUpdate();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
+
+public enum Direction
+{
+    PosY,
+    PosX,
+    NegY,
+    NegX
 }
